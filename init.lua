f3b38ec1484e5da3d17e35f145e66f5826460ed2
Paulo Artur <pauloarturvillaca@gmail.com>
2024-12-29 20:57:08 -0300

    add: elixir-lsp

--- init.lua
+++ init.lua
@@ -537,6 +537,7 @@ require('lazy').setup({
         pyright = {},
         rust_analyzer = {},
         texlab = {},
+        elixirls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
