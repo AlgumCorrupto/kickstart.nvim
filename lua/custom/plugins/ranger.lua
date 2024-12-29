84f2946f983f557f999a02f15f136f174ac7c7a4
Paulo Artur <pauloarturvillaca@gmail.com>
2024-12-29 18:05:49 -0300

    fix ranger

--- lua/custom/plugins/ranger.lua
+++ lua/custom/plugins/ranger.lua
@@ -1,7 +1,12 @@
 return {
   'kelly-lin/ranger.nvim',
   config = function()
-    require('ranger-nvim').setup { replace_netrw = true }
+    require('ranger-nvim').setup {
+      replace_netrw = true,
+      ui = {
+        height = 0.95,
+      },
+    }
     vim.api.nvim_set_keymap('n', '\\', '', {
       noremap = true,
       callback = function()
