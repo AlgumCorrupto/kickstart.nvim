dd1e47e3f14caa68feac7177ecb7cc8ee2fc0791
Paulo Artur <pauloarturvillaca@gmail.com>
2024-12-31 20:30:13 -0300

    add: luasnips

--- init.lua
+++ init.lua
@@ -654,15 +654,15 @@ require('lazy').setup({
           return 'make install_jsregexp'
         end)(),
         dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          --  `friendly-snippets` contains a variety of premade snippets.
+          --   See the README about individual language/framework/plugin snippets:
+          --   https://github.com/rafamadriz/friendly-snippets
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
       },
       'saadparwaiz1/cmp_luasnip',
@@ -671,6 +671,7 @@ require('lazy').setup({
       --  nvim-cmp does not ship with all sources by default. They are split
       --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
+      'saadparwaiz1/cmp_luasnip',
       'hrsh7th/cmp-path',
     },
     config = function()
